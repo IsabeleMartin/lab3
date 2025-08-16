d04b0d5965e3d4ea8b3bc29f90e38c671f54d3f6
IsabeleMartins <isabelemartinsnascimento15@gmail.com>
2025-08-16 10:58:04 -0400

    update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,17 @@
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-}
+import type { Config } from '@jest/types';
+
+const config: Config = {
+  preset: 'ts-jest',  // Usa o preset do ts-jest para compilar TypeScript
+  testEnvironment: 'node',  // Define o ambiente de teste para Node.js
+  transform: {
+    '^.+\\.ts$': 'ts-jest',  // Transforma arquivos .ts usando ts-jest
+  },
+  moduleFileExtensions: ['ts', 'js'],  // Extensões de arquivo que o Jest deve processar
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,  // Melhora o desempenho ao compilar cada arquivo separadamente
+    },
+  },
+};
+
+export default config;
