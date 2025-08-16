13a6e9bcd46ffea87b21595c19bcaf640a234dae
IsabeleMartins <isabelemartinsnascimento15@gmail.com>
2025-08-16 13:27:17 -0400

    downgrade jest to 29

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,10 @@
-export default {
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-}
+  transform: { '^.+\\.ts$': 'ts-jest' },
+  moduleFileExtensions: ['ts', 'js'],
+};
+
+export default config;
